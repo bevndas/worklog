20d954996cff396f9105bea77063a817f6a39a92
Bipin Shrestha <bipinshrestha.work@gmail.com>
2022-08-30 21:47:09 +0545

    Refactor: Formik input error case handling

--- src/components/Formik/FormikInput/FormikInput.tsx
+++ src/components/Formik/FormikInput/FormikInput.tsx
@@ -1,14 +1,13 @@
 import {Field, ErrorMessage, useField} from 'formik';
 import FormikErrorMessage from '../FormikErrorMessage';
 
-const FormikInput = ({name,label, ...props}: any) => {
-    console.log('props', props)
+const FormikInput = ({label, ...props}: any) => {
     const [field, meta] = useField(props);
     return (
-        <div className={`form-control ${meta.error && 'error'}`}>
-            <label htmlFor={name}>{label}</label>
-            <Field id={name} name={name} {...props} />
-            <ErrorMessage name={name} component={FormikErrorMessage}/>
+        <div className={`form-control ${meta.error && meta.touched && 'error'}`}>
+            <label htmlFor={field.name}>{label}</label>
+            <input {...field}/>
+            <ErrorMessage name={field.name} component={FormikErrorMessage}/>
 
         </div>
     )
