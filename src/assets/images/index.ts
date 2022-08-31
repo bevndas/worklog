50a026eb41b7ee42917de49042827d0081daa24b
Bipin Shrestha <bipin@studiomatrix.com.np>
2022-08-31 12:07:57 +0545

    Refactor: Images added

--- src/assets/images/index.ts
+++ src/assets/images/index.ts
@@ -1,13 +1,19 @@
 import LoginImage from './login.svg';
 import CheckpointImage from './checkpoint.svg';
+import ForgotPasswordImage from './forgot-password.svg';
 import SuccessImage from './success.svg';
 import TeamImage from './team.svg';
 import CongratulationsImage from './congratulations.svg';
+import ResetPasswordImage from './reset-password.svg';
+import MailSentImage from './mail-sent.svg';
 
 export {
     LoginImage,
     CheckpointImage,
     SuccessImage,
     TeamImage,
-    CongratulationsImage
+    CongratulationsImage,
+    ForgotPasswordImage,
+    ResetPasswordImage,
+    MailSentImage
 }
