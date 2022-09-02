2cf93cd1df3b485c471c02ee815ff9ec2849c64f
Bipin Shrestha <bipin@studiomatrix.com.np>
2022-09-02 15:33:24 +0545

    Feat: Add card component

---
+++ src/components/Cards/Card.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import './Card.scss';
+
+const Card= (props: any) => {
+    return (
+        <div className='card'>
+            {props.children}
+        </div>
+    )
+}
+export default Card;
